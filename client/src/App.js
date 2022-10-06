708b1ca9398a3f301a35159920735604b8940260
alexjoe118 <debrajoycel8323@gmail.com>
2022-10-06 13:42:36 -0400

    step2

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,26 @@
-import {useAuth} from './contexts/AuthContext'
 import Header from './components/Header'
-
+import TestingArea from './components/TestingArea'
+import Landing from './components/Landing'
 export default function App() {
-  const {isLoggedIn} = useAuth()
+  
 
   return (
     <div className='App'>
       <Header />
-      
-      {isLoggedIn ? <LoggedInText /> : <LoggedOutText />}
+      <Landing />
+      <TestingArea />
+      {/* <TestingArea></TestingArea> */}
+      {/* {isLoggedIn ? <LoggedInText /> : <LoggedOutText />} */}
     </div>
   )
 }
 
-const LoggedInText = () => {
-  const {account} = useAuth()
+// const LoggedInText = () => {
+//   const {account} = useAuth()
 
-  return <p>Hey, {account.username}! I'm happy to let you know: you are authenticated!</p>
-}
+//   return <p>Hey, {account.username}! I'm happy to let you know: you are authenticated!</p>
+// }
 
-const LoggedOutText = () => (
-  <p>Don't forget to start your backend server, then authenticate yourself.</p>
-)
+// const LoggedOutText = () => (
+//   <p>Don't forget to start your backend server, then authenticate yourself.</p>
+// )
